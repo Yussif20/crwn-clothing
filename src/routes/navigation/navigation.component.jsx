cc42e3f778daffcbb5da0ed0aadec63e4f0aeb62
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-12 09:50:28 +0200

    finalizing auth listener

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,11 +8,8 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  };
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -24,8 +21,8 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutHandler}>
-              Log out
+            <span className="nav-link" onClick={signOutUser}>
+              Sign out
             </span>
           ) : (
             <Link className="nav-link" to="/auth">
