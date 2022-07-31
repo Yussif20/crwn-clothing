64e8ec3ef1ea56d0721ce25e958e93fb904de220
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-31 22:06:42 +0200

    making the button on the product actually do something

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items">
-      {[].map((item) => (
-        <CartItem cartItem={item} />
-      ))}
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>CHECKOUT</Button>
     </div>
-    <Button>CHECKOUT</Button>
-  </div>
-);
+  );
+};
 
 export default CartDropdown;
