b2da1e65dcaf86a4e5596d78bcdb7497556aede5
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-31 21:54:37 +0200

    adding logic to cart context so it add items

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,8 +2,19 @@ import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   //check if cartitems contains products added
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
   //if found icreament quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
   //return new array with modified/new cartitems
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
 export const CartContext = createContext({
@@ -21,7 +32,7 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
