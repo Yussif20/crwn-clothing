0e64bc38ec3786ac10585584357e732b428df314
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-08-08 18:10:26 +0200

    nested routes in shop

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,18 +1,22 @@
-import "./category-preview.styles.scss";
+import { Link } from "react-router-dom";
 
 import ProductCard from "../product-card/product-card.component";
 
-const CategoryPreview = (title, products) => {
+import "./category-preview.styles.scss";
+
+const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-containere">
+    <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products
-          .filter((_, idx) => idx > 4)
+          .filter((_, idx) => idx < 4)
           .map((product) => (
-            <ProductCard key={product.id} product={product}></ProductCard>
+            <ProductCard key={product.id} product={product} />
           ))}
       </div>
     </div>
